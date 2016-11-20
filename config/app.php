02ed9c1f30ba86189c47dadc49216af80c3152f3
jjfs85 <john@scimone.net>
2016-11-19 21:25:25 -0500

    Slight changes to remove old scimone.net references.

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Scimone.net',
+    'name' => 'Spark.coop',
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://scimone.net'),
+    'url' => env('APP_URL', 'http://spark.coop'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2 text-center">
                 <h1>WELCOME</h1>
-                This is Scimone.net. Maybe I'll use it someday.
+                This is Spark Makerspace. Maybe this website will be in use someday.
             </div>
         </div>
     </div>
