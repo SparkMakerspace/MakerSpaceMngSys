6460c68f9c30228230a881bb59e17f9ffa403e19
jjfs85 <john@scimone.net>
2016-12-11 01:58:37 -0500

    A couple minor edits for a good responsive layout.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,74 +16,87 @@
 @endsection
 
 @section('content')
-    {{-- Posts List --}}
+    {{-- Holds the Post cards --}}
     <div class="w3-row-padding">
         @if($posts->isEmpty())
-            <div class="w3-card-2 w3-red w3-hover-dark-gray w3-margin-top w3-animate-right w3-container">
+            <div class="w3-card-2 w3-red w3-hover-dark-gray w3-margin-top w3-container">
                 <h4>Looks like there aren't any posts quite yet...</h4>
             </div>
         @else
             @foreach ($posts as $post)
-                <div class="w3-card-2 w3-light-gray w3-margin-top w3-half">
-                    <div class="w3-container w3-border-bottom w3-row">
+                @if(($loop->index+1) % 2 )
+                    </div><div class="w3-row-padding">
+                @endif
+                {{-- This div is the Post card --}}
+            <div class="w3-half">
+                <div class="w3-card-2 w3-light-gray w3-section">
 
-                        <div class="w3-col m1 w3-right w3-row">
+                    {{-- This div is for the Post header --}}
+                    <div class="w3-border-bottom w3-row w3-padding-4">
 
-                            <!-- Post Owner Actions -->
+                        {{-- This div is for the owner actions on the right of the header --}}
+                        <div class="w3-col m2 w3-right w3-btn-group">
                             @can('delete',$post)
-                                <div class="w3-col m6 w3-right w3-right-align">
-                                    <form action="{{ URL::route('posts.destroy',$post) }}" method="POST">
-                                        <div class="">
-                                            <input type="hidden" name="_method" value="DELETE">
-                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                            <button class="w3-btn w3-red w3-small w3-padding-small">
-                                                Delete
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
+                                <form action="{{ URL::route('posts.destroy',$post) }}" method="POST">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <button class="w3-btn w3-red w3-small w3-right w3-padding-small">
+                                        <i class="fa fa-times"></i>
+                                    </button>
+                                </form>
                             @endcan
                             @can('update',$post)
-                                <div class="w3-col m6 w3-right w3-right-align">
-                                    <form action="{{ URL::route('posts.edit',$post) }}">
-                                        <div class="">
-                                            <button class="w3-btn w3-blue-grey w3-small w3-padding-small">
-                                                Edit
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
+                                <form action="{{ URL::route('posts.edit',$post) }}">
+                                    <button class="w3-btn w3-blue-grey w3-small w3-right w3-padding-small">
+                                        <i class="fa fa-pencil-square-o"></i>
+                                    </button>
+                                </form>
                             @endcan
+                        {{-- /owner actions --}}
                         </div>
-                        <div class="w3-rest w3-row">
-                            <div class="w3-col m4 w3-right w3-right-align">
-                                by @if ($post->ownerExists())
-                                    {{$post->owner->name}}
-                                @else
-                                    Deleted User
-                                @endif
-                            </div>
-                            <div class="w3-rest">
-                                <a href="posts/{{$post->id}}">
+
+                        {{-- This div holds the Post title and owner name --}}
+                        <div class="w3-rest w3-row-padding">
+                            <div class="w3-col m8">
+                                <a href="posts/{{$post->id}}" class="w3-large">
                                     {{ $post->title}}
                                 </a>
                             </div>
+                            <div class="w3-col m4">
+                                <div class="w3-medium">
+                                    by @if ($post->ownerExists())
+                                        {{$post->owner->name}}
+                                    @else
+                                        Deleted User
+                                    @endif
+                                </div>
+                            </div>
+                        {{-- /Post title and owner name --}}
                         </div>
+                    {{-- /Post header --}}
                     </div>
 
                     <!-- Post Body -->
                     <div class="w3-container w3-white">
                         {!! html_entity_decode($post->body) !!}
+                    {{-- /Post Body --}}
                     </div>
+
+                    {{-- Post Footer --}}
                     <div  class=" w3-container w3-row w3-border-top">
-                        <div class="w3-rest"></div>
-                        <div class="w3-right w3-right-align w3-col m4">
+                        <div class="w3-right w3-right-align w3-col m6">
                             Posted: {{$post->post_time}}
                         </div>
+                        <div class="w3-rest"></div>
+                    {{-- /Post Footer --}}
                     </div>
+                {{-- /Post Card --}}
+                </div>
                 </div>
             @endforeach
         @endif
+
+    {{-- /Holds the Post cards --}}
     </div>
 @endsection
 
