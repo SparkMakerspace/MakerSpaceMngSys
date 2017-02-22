184aaa4ad4de33720dfa88b6c7236b9a7745dd82
mmiscool <mike@smbisoft.com>
2017-02-21 22:30:01 -0500

    Added more links to the home page to make testing easier.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,19 @@
 @section('someSideBarJunk')
     <li class="treeview"><a href="{{url('./event')}}"></i> <span>Calender</span></a></li>
     <li class="treeview"><a href="{{url('./g/')}}"></i> <span>Groups</span></a></li>
+    <li class="treeview"><a href="{{url('./posts/')}}"></i> <span>Post</span></a></li>
+    <li class="treeview"><a href="{{url('./resource/')}}"></i> <span>Resource</span></a></li>
+
+
+    <li class="treeview"><a href="{{url('./comment/')}}"></i> <span>Comments</span></a></li>
+
+    <li class="treeview"><a href="{{url('./Door/')}}"></i> <span>Doors</span></a></li>
+
+    <li class="treeview"><a href="{{url('./image/')}}"></i> <span>Images</span></a></li>
+
+    <li class="treeview"><a href="{{url('./resource/')}}"></i> <span>Resource</span></a></li>
+
+    <li class="treeview"><a href="{{url('./test/')}}"></i> <span>Test</span></a></li>
 
 @endsection
 
