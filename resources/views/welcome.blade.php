0ebb929b3aecb3cd1bec44df2075fcba2267ef33
jjfs85 <john@scimone.net>
2017-04-15 16:33:17 -0400

    Added a link on the frontpage to the Github issue tracker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,9 @@
 @section('content')
     <div class="content">
         <h1 class="title">
-            SPARK MAKERSPACE!!!! WOOOOOO!!!
-
-
-
-        </h1>
+            SPARK MAKERSPACE DEV SANDBOX
+        </h1><h2>
+            Please log any/all issues on our <a href="http://github.com/SparkMakerspace/MakerSpaceMngSys/issues">Github issue tracker</a>
+        </h2>
     </div>
 @endsection
