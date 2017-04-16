2f278831b6b3204c5b5ec6552e69e4bcb9b69d09
Drew Gates <drew@drewgates.com>
2017-04-15 21:25:18 -0400

    webmail updates
    setup.sh in this branch will not work.

--- routes/web.php
+++ routes/web.php
@@ -123,6 +123,20 @@ Route::group(['middleware'=> ['auth']],function() {
   Route::get('chore_list/{id}/deleteMsg','\App\Http\Controllers\Chore_listController@DeleteMsg');
 
 
+  //webmail routes
+  Route::get('/webmail',function()
+    {
+      if(Auth::check())
+        return File::get(public_path() . '/webmail/index.php');
+        return File::get(public_path() . '/webmail/index.php');
+    });
+
+    Route::get('/webmail/{slug}',function($slug)
+    {
+      if(Auth::check())
+          return File::get(public_path() . '/webmail/' . $slug);
+      return File::get(public_path() . '/webmail/' . $slug);
+    });
 
 
 //There is a reason these are at the bottom of the routes file.
--- setup.sh
+++ setup.sh
@@ -16,11 +16,18 @@ php artisan storage:link
 
 php artisan vendor:publish
 
-# wget https://github.com/roundcube/roundcubemail/releases/download/1.2.4/roundcubemail-1.2.4-complete.tar.gz -P public/webmail/
+wget https://github.com/roundcube/roundcubemail/releases/download/1.2.4/roundcubemail-1.2.4-complete.tar.gz -P public/webmail/
 
-# tar -xvf public/webmail/roundcubemail-1.2.4-complete.tar.gz
+tar -xvf public/webmail/roundcubemail-1.2.4-complete.tar.gz -C public/webmail
 
-# rm public/webmail/roundcubemail-1.2.4-complete.tar.gz
+rm public/webmail/roundcubemail-1.2.4-complete.tar.gz
+
+rsync -a public/webmail/roundcubemail-1.2.4/ public/webmail/
+
+# mv public/webmail/roundcubemail-1.2.4/* public/webmail/
+# mv public/webmail/roundcubemail-1.2.4/.* public/webmail/
+
+
 
 # script for roundcube installation
 # https://gist.githubusercontent.com/rcubetrac/cc85589b837d58680a86e7b5cbb09a4f/raw/6a04577ae65c9a035404ea46f5861c939558c248/debian_install_mysql.sh%25E2%2580%258B
