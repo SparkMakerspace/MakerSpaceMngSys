801ffea37a8ba35c3b6e60e96d46822c0d35418f
jjfs85 <john@scimone.net>
2017-05-02 20:38:37 -0400

    fixed share buttons!!!

--- config/app.php
+++ config/app.php
@@ -232,7 +232,7 @@ return [
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
-        'Share' => BrianFaust\SocialShare\Facades\SocialShare,
+        'Share' => BrianFaust\SocialShare\Facades\SocialShare::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'URL' => Illuminate\Support\Facades\URL::class,
