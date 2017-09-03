c5dd266a15e19ee64653141440f6b84801b65560
Drew Gates <me@drewgates.com>
2017-09-03 14:02:54 -0400

    linux dependency installer updates
    linux dependency installer updates

--- linux-setup-requirements.sh
+++ linux-setup-requirements.sh
@@ -2,11 +2,11 @@
 sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
-apt -y install nginx git php7.1 php7.1-mysql php7.1-fpm php7.1-mbstring php7.1-xml php7.1-curl mysql-server
+apt -y install nginx git php7.1 php7.1-mysql php7.1-fpm php7.1-mbstring php7.1-xml php7.1-curl php7.1-cli mysql-server freeglut3 slic3r g++ make
 mkdir -p /var/www/html
 rm -rf /var/www/html/*
 git clone https://github.com/SparkMakerspace/MakerSpaceMngSys.git /var/www/html/
-mv ./linux.nginx.default.conf /etc/nginx/conf.d/default.conf
+cp ./linux.nginx.default.conf /etc/nginx/conf.d/default.conf
 sed -i 's/localhost/nonstop3d.com/g' /etc/nginx/conf.d/default.conf
 echo ‘cgi.fix_pathinfo=0’ >> /etc/php/7.1/fpm/php.ini
 /etc/init.d/nginx restart
@@ -16,3 +16,9 @@ mv composer.phar /usr/local/bin/composer
 cd /var/www/html
 chown -R www-data:www-data /var/www/html
 chmod -R 775 /var/www/html/storage
+wget https://nodejs.org/dist/v6.11.2/node-v6.11.2.tar.gz
+tar -xvf node-v6*
+cd node*
+./configure
+make
+sudo make install
