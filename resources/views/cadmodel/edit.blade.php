68289966589911adf428d264e4633cc4a4ffd56d
mmiscool <mike@smbisoft.com>
2017-09-06 01:06:12 -0400

    saving and loading of cad model file implimented. Ajax used for real time saving.

--- resources/views/cadmodel/edit.blade.php
+++ resources/views/cadmodel/edit.blade.php
@@ -2,152 +2,154 @@
 @section('title','Edit')
 @section('content')
 
-    <body onload="updatelistbox();">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <div class="panel-group" id="accordion">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Group Memberships</a>
-                </h4>
-            </div>
-            <div id="collapse1" class="panel-collapse collapse">
-                <div class="panel-body">
+    <section class="content">
+        <div class="panel-group" id="accordion">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Model Properties.</a>
+                    </h4>
+                </div>
+                <div id="collapse1" class="panel-collapse collapse">
+                    <div class="panel-body">
 
 
-                    <form method = 'get' action = '{!!url("cadmodel")!!}'>
-                        <button class = 'btn btn-danger'>cadmodel Index</button>
-                    </form>
-                    <br>
-                    <form method = 'POST' action = '{!! url("cadmodel")!!}/{!!$cadmodel->
+                        <form method='get' action='{!!url("cadmodel")!!}'>
+                            <button class='btn btn-danger'>cadmodel Index</button>
+                        </form>
+                        <br>
+                        <form method='POST' action='{!! url("cadmodel")!!}/{!!$cadmodel->
         id!!}/update'>
-                        <input type = 'hidden' name = '_token' value = '{{Session::token()}}'>
-                        <div class="form-group">
-                            <label for="Name">Name</label>
-                            <input id="Name" name = "Name" type="text" class="form-control" value="{!!$cadmodel->
+                            <input type='hidden' name='_token' value='{{Session::token()}}'>
+                            <div class="form-group">
+                                <label for="Name">Name</label>
+                                <input id="Name" name="Name" type="text" class="form-control" value="{!!$cadmodel->
             Name!!}">
-                        </div>
-                        <div class="form-group">
-                            <label for="Description">Description</label>
-                            <input id="Description" name = "Description" type="text" class="form-control" value="{!!$cadmodel->
+                            </div>
+                            <div class="form-group">
+                                <label for="Description">Description</label>
+                                <input id="Description" name="Description" type="text" class="form-control" value="{!!$cadmodel->
             Description!!}">
-                        </div>
-                        <div class="form-group">
-                            <label for="ModelFile">ModelFile</label>
-                            <textarea id="ModelFile" name = "ModelFile" type="textarea"  >{!!$cadmodel->ModelFile!!}</textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="Material">Material</label>
-                            <input id="Material" name = "Material" type="text" class="form-control" value="{!!$cadmodel->
+                            </div>
+                            <div class="form-group">
+                                <label for="ModelFile">ModelFile</label>
+                                <textarea id="ModelFile" name="ModelFile"
+                                          type="textarea">{!!$cadmodel->ModelFile!!}</textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="Material">Material</label>
+                                <input id="Material" name="Material" type="text" class="form-control" value="{!!$cadmodel->
             Material!!}">
-                        </div>
-                        <button class = 'btn btn-primary' type ='submit'>Update</button>
-                    </form>
+                            </div>
+                            <button class='btn btn-primary' type='submit'>Update</button>
+                        </form>
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
+        <script>
 
+            readSingleFile();
 
-
-
-
-
-
-
-    <script>
-        function listKeys()
-        {
-            alert("grabling all the file for the current file");
-            var keyIndex = 0;
-            var thisKey = window.localStorage.key(keyIndex);
-
-            while(thisKey != '' && thisKey != undefined)
-            {
-                if (thisKey.substr(0, 2) == 'TCAD.projects.')
-                {
-                    alert(thisKey);
+            function purgelocalfile() {
+                var bla = [];
+                for (var i = 0, len = localStorage.length; i < len; i++) {
+                    if (localStorage.key(i).substr(0, {!! strlen('TCAD.projects.'. $cadmodel->id) !!}) == '{!! 'TCAD.projects.'. $cadmodel->id !!}') {
+                        bla[i] = localStorage.key(i);
+                    }
                 }
 
-                keyIndex+= 1;
-                thisKey = window.localStorage.key(keyIndex);
-            }
-        }
+                len = localStorage.length
+                for (var i = 0; i < len; i++) {
 
+                        localStorage.removeItem(bla[i]);
 
-
-
-        function ExportToLocalFile()
-        {
-            alert("hello ");
-            var bla;
-            var lengthOfLocalStorage = localStorage.length;
-            bla = lengthOfLocalStorage.toString();
-            for(var i=0, len=localStorage.length; i<len; i++)
-            {
-                if (localStorage.key(i).substr(0, {!! strlen('TCAD.projects.'. $cadmodel->id) !!}) == '{!! 'TCAD.projects.'. $cadmodel->id !!}')
-                {
-
-                    var key = localStorage.key(i);
-                    var value = localStorage[key];
-
-                    bla += "\n" + key +"\n" + value;
                 }
 
             }
 
-            alert( localStorage[key]);
-            $('#ModelFile').val(bla);
-            //exportTextData(bla,"export.web-cad");
-        }
+            function readSingleFile(e) {
+                reading = 1;
+                purgelocalfile();
+                var arrayOfLines = $('#ModelFile').val().split("\n");
+                //alert(arrayOfLines[0]);
 
-        //localStorage.setItem("bla","blabla");
+                for (var i = 0; i < arrayOfLines[0]; i++) {
+                    localStorage.setItem(arrayOfLines[i * 2 + 1], arrayOfLines[i * 2 + 2]);
+                }
+                reading = 0;
+            }
 
-        $(function () {
 
-            $(window).bind('storage', function (e) {
-                ExportToLocalFile();
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
             });
 
-            localStorage.setItem('a', 'test');
 
-        });
+            function ExportToLocalFile() {
+                if (reading == 1) return;
+                var bla;
+                var lengthOfLocalStorage = localStorage.length;
+                bla = lengthOfLocalStorage.toString();
+                for (var i = 0, len = localStorage.length; i < len; i++) {
+                    if (localStorage.key(i).substr(0, {!! strlen('TCAD.projects.'. $cadmodel->id) !!}) == '{!! 'TCAD.projects.'. $cadmodel->id !!}') {
 
+                        var key = localStorage.key(i);
+                        var value = localStorage[key];
 
-        function readSingleFile(e) {
-            var file = e.target.files[0];
-            if (!file) {
-                return;
-            }
-            var reader = new FileReader();
-            reader.onload = function(e)
-            {
-                var data = e.target.result;
-                var arrayOfLines = data.split("\n");
-                alert(arrayOfLines[0]);
+                        bla += "\n" + key + "\n" + value;
+                    }
 
-                for (var i = 0; i < arrayOfLines[0]; i++)
-                {
-                    localStorage.setItem(arrayOfLines[i*2+1],arrayOfLines[i*2+2]);
                 }
-            };
-            reader.readAsText(file);
-        }
+
+                //alert(localStorage[key]);
+                $('#ModelFile').val(bla);
+                $.post( "updatemodel", $( "#ModelFile" ).serialize() );
+                //exportTextData(bla,"export.web-cad");
+            }
 
 
-        ExportToLocalFile();
-
-    </script>
 
 
-<iframe src="../../../../../webcad/?{!!$cadmodel->id!!}}" style="position: absolute;
 
-bottom: 0;
-height:85%;
-width:85%;"></iframe>
+
+            $(function () {
+
+                $(window).bind('storage', function (e) {
+                    ExportToLocalFile();
+                });
+
+                localStorage.setItem('a', 'test');
+
+            });
+
+
+
+
+            //ExportToLocalFile();
+            $(document).ready(SetIframeSize);
+
+            // resize on window resize
+            $(window).on('resize', SetIframeSize);
+
+            function SetIframeSize() {
+                $("#cad").width($(window).width() - 275); // added margin for scrollbars
+                $("#cad").height($(window).height() - 150);
+            }
+
+
+        </script>
+
+        <iframe id='cad' src="../../../../../webcad/?{!!$cadmodel->id!!}}" onload="resizeIframe(this)"></iframe>
+    </section>
 
 
 @endsection
